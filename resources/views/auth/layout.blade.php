4b8a682a29afc47e8d2b21e49a5abc01252cb993
Vimal Patel <vimalascetic@gmail.com>
2022-06-20 07:36:42 +0530

    Set basic assets path for login layout

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('login-assets/img/apple-icon.png')}}">
-        <link rel="icon" type="image/png" href="{{ asset('login-assets/img/favicon.png')}}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png')}}">
+        <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png')}}">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <title>
             My Politics
@@ -13,9 +13,9 @@
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
         <!-- CSS Files -->
-        <link href="{{ asset('login-assets/css/bootstrap.min.css')}}" rel="stylesheet" />
-        <link href="{{ asset('login-assets/css/paper-dashboard.min.css')}}" rel="stylesheet" />
-        <link href="{{ asset('login-assets/css/style.css')}}" rel="stylesheet" />
+        <link href="{{ asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" />
+        <link href="{{ asset('assets/css/paper-dashboard.min.css')}}" rel="stylesheet" />
+        <link href="{{ asset('assets/css/style.css')}}" rel="stylesheet" />
     </head>
     <body class="page-login">
         <div class="wrapper ">
@@ -42,6 +42,6 @@
         </div>
     </body>
     <!--   Core JS Files   -->
-    <script src="{{ asset('login-assets/js/jquery.min.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery.min.js')}}"></script>
 </html>
 
