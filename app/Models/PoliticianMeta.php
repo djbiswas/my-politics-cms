cdb3dae65b5788d9f9de6214e2bfd13b50cc422d
khanjan-devuforia <kukadiya.harshadbhai@devuforia.com>
2022-06-28 07:15:38 +0530

    small changes

---
+++ app/Models/PoliticianMeta.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+
+class PoliticianMeta extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'politician_id',
+        'type',
+        'key',
+        'value'
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    /**
+     *
+     * @param type $query
+     * @return type Illuminate\Support\Collection
+     */
+    public function scopeOnlyActive($query)
+    {
+        return $query->where('status', config('constants.status.active'));
+    }
+}
--- app/Models/UserMeta.php
+++ app/Models/UserMeta.php
@@ -4,14 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserMeta extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
-        'meta_key',
-        'meta_value'
+        'type',
+        'key',
+        'value'
     ];
 }
