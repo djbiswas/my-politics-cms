9083889bc554cd79e313601dbe16920ff9f1f526
khanjan-devuforia <kukadiya-harshadbhai@devuforia.com>
2022-06-30 08:53:14 +0530

    Import Category & Rank Data

---
+++ app/Console/Commands/ImportData.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+class ImportData extends Command
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import the older data from older database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->alert($this->description);
+        $this->call('import:category');
+        $this->call('import:rank');
+    }
+}
