06fcc09d813cc750edb90baf5398bf8f9112c36c
khanjan-devuforia <kukadiya.harshadbhai@devuforia.com>
2022-07-11 14:07:19 +0530

    CORS Error resolved

--- public/uploads.php
+++ public/uploads.php
@@ -8,12 +8,11 @@ error_reporting(E_ALL);
 // header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization,X-Requested-With');
 // header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
 
-header("Access-Control-Allow-Origin: https://devuforiawork239.site");
+// header("Access-Control-Allow-Origin: https://devuforiawork239.site");
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: *");
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization,X-Requested-With');
-header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
+// header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+// header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
 
 $target_dir = "/var/www/clone/my-politics-cms/storage/app/public/post/";
 $target_file = $target_dir . basename($_FILES["file"]["name"]);
